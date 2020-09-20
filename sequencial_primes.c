bf81e25bfd5b3eac7b07f38f2cf72fd5e30cf479
Gabriel Donadel Dall'Agnol <gabriel@jungledevs.com>
2020-09-20 18:08:02 -0300

    perf: Improve sequencial performance

--- sequencial_primes.c
+++ sequencial_primes.c
@@ -1,43 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
 #include <errno.h>
 #include <time.h>
 
 int is_prime(int n)
 {
-	for(int i = 2; i < n; i++){
+	int limit = n/2;
+	for(int i = 2; i <= limit; i++){
 		if(n%i == 0){
 			return 0;
 		}
 	}
-	
+
 	return 1;
 }
 
 void evaluate_numbers(int max_number){
 	int counter = 0;
-	
-	for(int i = 1; i <= max_number; i++){
+
+	for(int i = 2; i <= max_number; i++){
 		if(is_prime(i)){
 			if(counter){
 				printf(",%d", i);
 			}else{
 				printf("Numbers: %d", i);
 			}
-			counter++; 
+			counter++;
 		}
 	}
-	
+
 	if(counter){
-		printf("\n\nTotal of prime numbers found: %d\n", counter); 
+		printf("\n\nTotal of prime numbers found: %d\n", counter);
 	}
 }
 
 int main(int argc, char *argv[])
-{ 
+{
 	clock_t t;
-    int max_number;  
-	char *p; 
+	int max_number;
+	char *p;
 
 	errno = 0;
 	long conv = strtol(argv[1], &p, 10);
@@ -49,15 +51,15 @@ int main(int argc, char *argv[])
 		// an error message
 		printf("Digite um numero valido!");
 		return 0;
-	} else { 
-		max_number = conv;     
+	} else {
+		max_number = conv;
 	}
-	
-	t = clock(); 
-	evaluate_numbers(max_number); 
+
+	t = clock();
+	evaluate_numbers(max_number);
 	t = clock() - t;
 	double time_taken = ((double)t)/CLOCKS_PER_SEC;
 	printf("Elapsed time to evaluate %d numbers: %fs\n", max_number,time_taken);
-	
-    return 0;
+
+	return 0;
 }
