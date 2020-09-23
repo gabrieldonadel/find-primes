5943f99e1f0712d7b27935ec180aabd0108fb695
Gabriel Donadel Dall'Agnol <gabriel@jungledevs.com>
2020-09-23 20:49:50 -0300

    feat: Improve sieve of eratosthenes performance using only odd numbers

--- sieve_of_eratosthenes.c
+++ sieve_of_eratosthenes.c
@@ -2,20 +2,21 @@
 #include <errno.h>
 #include <limits.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 int main(int argc, char *argv[])
 {
 
-  int max_number, i, j, counter;
-  char *p;
+  int max_number, i, j, counter, verbose;
+  char *param1;
 
   errno = 0;
-  long conv = strtol(argv[1], &p, 10);
+  long conv = strtol(argv[1], &param1, 10);
 
-  if (errno != 0 || *p != '\0' || conv > INT_MAX)
+  if (errno != 0 || *param1 != '\0' || conv > INT_MAX - 1)
   {
-    printf("Digite um numero menor que %d!/n", INT_MAX);
+    printf("You must type in a number smaller then %d!\n", INT_MAX);
     return 0;
   }
   else
@@ -23,46 +24,78 @@ int main(int argc, char *argv[])
     max_number = conv;
   }
 
-  clock_t t = clock();
-  int *primes;
-  primes = (int *)malloc(sizeof(int) * max_number);
-
-  //populating array with naturals numbers
-  primes[2] = 2;
-  for (i = 3; i <= max_number; i = i + 2)
-    primes[i] = i;
-
-  i = 2;
-  while ((i * i) <= max_number)
+  if (argc >= 1)
   {
-    if (primes[i] != 0)
+    for (int i = 1; i < argc; i++)
     {
-      for (j = 2; j < max_number; j++)
+      if (strcmp(argv[i], "-v") == 0)
       {
-        if (primes[i] * j > max_number)
-          break;
-        else
-          // Instead of deleteing , making elemnets 0
-          primes[primes[i] * j] = 0;
+        verbose = 1;
       }
     }
-    i++;
   }
 
-  for (i = 2; i <= max_number; i++)
+  clock_t t = clock();
+  int array_size = (max_number - 1) / 2;
+  unsigned char *primes = malloc(sizeof(unsigned char) * (array_size + 1));
+
+  //Initializing the array
+  for (i = 0; i <= array_size; i++)
+    primes[i] = 0x01;
+
+  //Iterating over array
+  for (i = 3; (i * i) <= max_number; i = i + 2)
   {
-    if (primes[i] != 0)
+    if (primes[i / 2] != 0)
     {
-      counter++;
-      printf(",%d", primes[i]);
+      for (j = i * i; j <= max_number; j = j + 2 * i)
+      {
+        // Set non prime numbers as 0x00
+        primes[j / 2] = 0x00;
+      }
     }
   }
 
-  printf("\n\nTotal of prime numbers found: %d\n", counter);
+  if (max_number >= 2)
+  {
+    counter++;
+  }
+
+  // Print all prime numbers found
+  if (verbose && max_number >= 2)
+  {
+    printf("Numbers: 2");
+
+    for (i = 1; i <= array_size; i++)
+    {
+      if (primes[i] != 0x00)
+      {
+        counter++;
+        printf(",%d", 2 * i + 1);
+      }
+    }
+    printf("\n");
+  }
+  else
+  {
+    for (i = 1; i <= array_size; i++)
+      counter += primes[i];
+  }
 
   t = clock() - t;
   double time_taken = ((double)t) / CLOCKS_PER_SEC;
-  printf("Elapsed time to evaluate %d numbers: %fs\n", max_number, time_taken);
+  printf("\033[0;32m");
+  printf("\n #################################################");
+  printf("\n %-36s%11d%2s", "# Total of numbers evaluated: ", max_number, " #");
+  printf("\n %-36s%11d%2s", "# Total of prime numbers found: ", counter, " #");
+  printf("\n %-36s%10fs%2s", "# Elapsed time to evaluate numbers: ", time_taken, " #");
+  printf("\n #################################################\n\n");
+  printf("\033[0m");
+
+  if (!verbose)
+  {
+    printf("Tip: Running this program using the flag -v will print all prime numbers found\n\n");
+  }
 
   return 0;
 }
