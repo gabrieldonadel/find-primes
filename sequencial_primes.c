b493617838ada05de49b76f009dc369fbb55f99a
Gabriel Donadel Dall'Agnol <gabriel@jungledevs.com>
2020-09-24 20:32:15 -0300

    feat: Update Sequencial algorithm

--- sequencial_primes.c
+++ sequencial_primes.c
@@ -4,11 +4,15 @@
 #include <errno.h>
 #include <time.h>
 
+int verbose = 0;
+
 int is_prime(int n)
 {
-	int limit = n/2;
-	for(int i = 2; i <= limit; i++){
-		if(n%i == 0){
+	int limit = n / 2;
+	for (int i = 2; i <= limit; i++)
+	{
+		if (n % i == 0)
+		{
 			return 0;
 		}
 	}
@@ -16,23 +20,42 @@ int is_prime(int n)
 	return 1;
 }
 
-void evaluate_numbers(int max_number){
+void verbosely_evaluate_numbers(int max_number)
+{
 	int counter = 0;
 
-	for(int i = 2; i <= max_number; i++){
-		if(is_prime(i)){
-			if(counter){
-				printf(",%d", i);
-			}else{
-				printf("Numbers: %d", i);
-			}
+	if (max_number > 1)
+	{
+		printf("Numbers: 2");
+		counter++;
+	}
+
+	for (int i = 3; i <= max_number; i = i + 2)
+	{
+		if (is_prime(i))
+		{
+			printf(",%d", i);
 			counter++;
 		}
 	}
 
-	if(counter){
-		printf("\n\nTotal of prime numbers found: %d\n", counter);
+	printf("\n\nTotal of prime numbers found: %d\n", counter);
+}
+
+void evaluate_numbers(int max_number)
+{
+	int counter = 0;
+
+	if (max_number > 1)
+		counter++;
+
+	for (int i = 3; i <= max_number; i = i + 2)
+	{
+		if (is_prime(i))
+			counter++;
 	}
+
+	printf("\n\nTotal of prime numbers found: %d\n", counter);
 }
 
 int main(int argc, char *argv[])
@@ -44,22 +67,29 @@ int main(int argc, char *argv[])
 	errno = 0;
 	long conv = strtol(argv[1], &p, 10);
 
-	// Check for errors: e.g., the string does not represent an integer
-	// or the integer is larger than int
-	if (errno != 0 || *p != '\0' || conv > INT_MAX) {
-		// Put here the handling of the error, like exiting the program with
-		// an error message
+	if (errno != 0 || *p != '\0' || conv > INT_MAX)
+	{
 		printf("Digite um numero valido!");
 		return 0;
-	} else {
+	}
+	else
+	{
 		max_number = conv;
 	}
 
 	t = clock();
-	evaluate_numbers(max_number);
+	if (verbose)
+	{
+
+		evaluate_numbers(max_number);
+	}
+	else
+	{
+		verbosely_evaluate_numbers(max_number);
+	}
 	t = clock() - t;
-	double time_taken = ((double)t)/CLOCKS_PER_SEC;
-	printf("Elapsed time to evaluate %d numbers: %fs\n", max_number,time_taken);
+	double time_taken = ((double)t) / CLOCKS_PER_SEC;
+	printf("Elapsed time to evaluate %d numbers: %fs\n", max_number, time_taken);
 
 	return 0;
 }
