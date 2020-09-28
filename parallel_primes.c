7cfb60fc8a5a7266649438d6185b4b18006564a8
Gabriel Donadel Dall'Agnol <gabriel@jungledevs.com>
2020-09-28 18:54:35 -0300

    feat: Update parallel primes algo

--- parallel_primes.c
+++ parallel_primes.c
@@ -3,26 +3,50 @@
 #include <limits.h>
 #include <errno.h>
 #include <time.h>
+#include <string.h>
 #include <pthread.h>
 
-struct set_of_numbers
-{
-  int min;
-  int max;
-};
+int n_threads, verbose;
 
 struct thread_custom_set
 {
-  int total_threads;
   int t_index;
   int max;
   int primes_found;
 };
 
+void print_results(int max_number, int counter, double time_taken)
+{
+  printf("\033[0;32m");
+  printf("\n\n #################################################");
+  printf("\n %-15s%18s%16s", "# ", "Standard Algorithm", " #");
+  printf("\n %-14s%21s%14s", "# ", "Parallel Prime Finder", " #");
+  printf("\n %-12s%26s%11s", "# ", "Gabriel Donadel Dall'Agnol", " #");
+  printf("\n #################################################");
+  printf("\n #%47s#", " ");
+  printf("\n %-36s%11d%2s", "# Number of threads used: ", n_threads, " #");
+  printf("\n %-36s%11d%2s", "# Total of numbers evaluated: ", max_number, " #");
+  printf("\n %-36s%11d%2s", "# Total of prime numbers found: ", counter, " #");
+  printf("\n %-36s%10fs%2s", "# Elapsed time to evaluate numbers: ", time_taken, " #");
+  printf("\n #%47s#", " ");
+  printf("\n #################################################\n\n");
+  printf("\033[0m");
+
+  if (!verbose)
+  {
+    printf(" Tip: Running this program using the flag -v will print all prime numbers found\n\n");
+  }
+}
+
 int is_prime(int n)
 {
   int limit = n / 2;
-  for (int i = 2; i <= limit; i++)
+  if (n % 2 == 0)
+  {
+    return 0;
+  }
+
+  for (int i = 3; i <= limit; i = i + 2)
   {
     if (n % i == 0)
     {
@@ -36,27 +60,33 @@ int is_prime(int n)
 void *custom_evaluate_numbers(void *args)
 {
   struct thread_custom_set *set = (struct thread_custom_set *)args;
-  int total_threads = set->total_threads;
   int t_index = set->t_index;
   int max = set->max;
-  int counter = 0;
+  int t_counter = 0;
 
-  if (t_index == 1)
+  if (verbose)
   {
-    t_index += total_threads;
-  }
-
-  for (int i = t_index; i <= max; i = i + total_threads)
-  {
-    if (is_prime(i))
+    for (int i = t_index; i <= max; i = i + (n_threads * 2))
     {
-      printf(",%d", i);
-
-      counter++;
+      if (is_prime(i))
+      {
+        printf(",%d", i);
+        t_counter++;
+      }
+    }
+  }
+  else
+  {
+    for (int i = t_index; i <= max; i = i + (n_threads * 2))
+    {
+      if (is_prime(i))
+      {
+        t_counter++;
+      }
     }
   }
 
-  set->primes_found = counter;
+  set->primes_found = t_counter;
   pthread_exit(NULL);
 }
 
@@ -64,7 +94,7 @@ int main(int argc, char *argv[])
 {
   clock_t t;
 
-  int max_number, n_threads, counter, i;
+  int max_number, counter, i;
 
   char *p1;
   char *p2;
@@ -84,6 +114,17 @@ int main(int argc, char *argv[])
     n_threads = conv2;
   }
 
+  if (argc >= 3)
+  {
+    for (int i = 2; i < argc; i++)
+    {
+      if (strcmp(argv[i], "-v") == 0)
+      {
+        verbose = 1;
+      }
+    }
+  }
+
   t = clock();
 
   pthread_t threads[n_threads];
@@ -91,16 +132,18 @@ int main(int argc, char *argv[])
 
   if (max_number > 1)
   {
-    printf("Numbers: ");
+    counter++;
+    if (verbose)
+    {
+      printf("Numbers: 2");
+    }
   }
 
-  for (i = 0; i < n_threads; i++)
+  for (i = 1; i <= n_threads; i++)
   {
-
-    sets[i].t_index = i + 1;
-    sets[i].max = max_number;
-    sets[i].total_threads = n_threads;
-    pthread_create(&threads[i], NULL, custom_evaluate_numbers, (void *)&sets[i]);
+    sets[i - 1].t_index = (i * 2) + 1;
+    sets[i - 1].max = max_number;
+    pthread_create(&threads[i - 1], NULL, custom_evaluate_numbers, (void *)&sets[i - 1]);
   }
 
   for (i = 0; i < n_threads; i++)
@@ -111,10 +154,12 @@ int main(int argc, char *argv[])
     counter += sets[i].primes_found;
   }
 
+  free(sets);
+
   t = clock() - t;
   double time_taken = ((double)t) / CLOCKS_PER_SEC;
-  printf("\n\nTotal of prime numbers found: %d", counter);
-  printf("\nElapsed time to evaluate %d numbers: %fs\n", max_number, time_taken);
+
+  print_results(max_number, counter, time_taken);
 
   return 0;
 }
