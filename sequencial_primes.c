86a1658cf9d1a2d30e462b423cc8359a39a202a1
Gabriel Donadel Dall'Agnol <gabriel@jungledevs.com>
2020-09-27 23:31:56 -0300

    feat: Update sequencial primes standard algo

--- sequencial_primes.c
+++ sequencial_primes.c
@@ -3,13 +3,40 @@
 #include <limits.h>
 #include <errno.h>
 #include <time.h>
+#include <string.h>
 
-int verbose = 0;
+int verbose, counter = 0;
+
+void print_results(int max_number, double time_taken)
+{
+	printf("\033[0;32m");
+	printf("\n\n #################################################");
+	printf("\n %-15s%18s%16s", "# ", "Standard Algorithm", " #");
+	printf("\n %-13s%23s%13s", "# ", "Sequencial Prime Finder", " #");
+	printf("\n %-12s%26s%11s", "# ", "Gabriel Donadel Dall'Agnol", " #");
+	printf("\n #################################################");
+	printf("\n #%47s#", " ");
+	printf("\n %-36s%11d%2s", "# Total of numbers evaluated: ", max_number, " #");
+	printf("\n %-36s%11d%2s", "# Total of prime numbers found: ", counter, " #");
+	printf("\n %-36s%10fs%2s", "# Elapsed time to evaluate numbers: ", time_taken, " #");
+	printf("\n #%47s#", " ");
+	printf("\n #################################################\n\n");
+	printf("\033[0m");
+
+	if (!verbose)
+	{
+		printf(" Tip: Running this program using the flag -v will print all prime numbers found\n\n");
+	}
+}
 
 int is_prime(int n)
 {
 	int limit = n / 2;
-	for (int i = 2; i <= limit; i++)
+	if (n % 2 == 0)
+	{
+		return 0;
+	}
+	for (int i = 3; i <= limit; i = i + 2)
 	{
 		if (n % i == 0)
 		{
@@ -20,42 +47,37 @@ int is_prime(int n)
 	return 1;
 }
 
-void verbosely_evaluate_numbers(int max_number)
+void evaluate_numbers(int max_number)
 {
-	int counter = 0;
-
 	if (max_number > 1)
 	{
-		printf("Numbers: 2");
 		counter++;
-	}
-
-	for (int i = 3; i <= max_number; i = i + 2)
-	{
-		if (is_prime(i))
+		if (verbose)
 		{
-			printf(",%d", i);
-			counter++;
+			printf("\nNumbers: 2");
 		}
 	}
 
-	printf("\n\nTotal of prime numbers found: %d\n", counter);
-}
-
-void evaluate_numbers(int max_number)
-{
-	int counter = 0;
-
-	if (max_number > 1)
-		counter++;
-
-	for (int i = 3; i <= max_number; i = i + 2)
+	if (verbose)
 	{
-		if (is_prime(i))
-			counter++;
+		for (int i = 3; i <= max_number; i = i + 2)
+		{
+			if (is_prime(i))
+			{
+				printf(",%d", i);
+				counter++;
+			}
+		}
 	}
+	else
+	{
 
-	printf("\n\nTotal of prime numbers found: %d\n", counter);
+		for (int i = 3; i <= max_number; i = i + 2)
+		{
+			if (is_prime(i))
+				counter++;
+		}
+	}
 }
 
 int main(int argc, char *argv[])
@@ -77,19 +99,23 @@ int main(int argc, char *argv[])
 		max_number = conv;
 	}
 
-	t = clock();
-	if (verbose)
+	if (argc >= 1)
 	{
+		for (int i = 1; i < argc; i++)
+		{
+			if (strcmp(argv[i], "-v") == 0)
+			{
+				verbose = 1;
+			}
+		}
+	}
 
-		evaluate_numbers(max_number);
-	}
-	else
-	{
-		verbosely_evaluate_numbers(max_number);
-	}
+	t = clock();
+	evaluate_numbers(max_number);
 	t = clock() - t;
 	double time_taken = ((double)t) / CLOCKS_PER_SEC;
-	printf("Elapsed time to evaluate %d numbers: %fs\n", max_number, time_taken);
+
+	print_results(max_number, time_taken);
 
 	return 0;
 }
