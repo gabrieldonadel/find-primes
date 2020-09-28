1eea9d01e0b230ddb827a37df715263b1b878cc7
Gabriel Donadel Dall'Agnol <gabriel@jungledevs.com>
2020-09-27 23:05:15 -0300

    feat: Update sieve of eratosthenes

--- sieve_of_eratosthenes.c
+++ sieve_of_eratosthenes.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 {
   if (argc == 1)
   {
-    printf("The first param must be a number!\n");
+    printf("The first argument must be a number!\n");
     return 0;
   }
 
